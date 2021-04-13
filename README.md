e4d8f521d1c1c7c76a9d2a38218c91ebaf527e10
weinen-j <80161559+weinen-j@users.noreply.github.com>
2021-04-13 17:33:19 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scrape_youtube_comments
  
+Access the YouTube Data Api to download the comments of one or mutliple videos based on the respective VideoID. 
+Currently, 10 comments are scraped per video, as well as up to one response per comment.
+At the end, the comments are stored in a csv file. The individual columns are separated by a "§".
+
+For further information, please refer to the official API documentation:
+https://developers.google.com/youtube/v3
+
--- commenttread.py
+++ commenttread.py
@@ -9,6 +9,7 @@ import csv
 import googleapiclient.discovery
 import pandas as pd
 
+
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
@@ -16,18 +17,19 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = " enter your developer key here "
-    video_id = {#"enter the video ID's here in the format 'ID'}
+    DEVELOPER_KEY = "AIzaSyBl1LoyjrbxOhv7fmUR2Is_PkbQk-_EbbM"
+    video_id = {"2xlol-SNQRU"}
 
     commentthread = []
+
     for i in video_id:
-        
+
         youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, developerKey = DEVELOPER_KEY)
+            api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
         request = youtube.commentThreads().list(
             part="snippet",
-            maxResults= 10,
+            maxResults=10,
             order="relevance",
             textFormat="plainText",
             videoId=i
@@ -36,48 +38,134 @@ def main():
         print(response)
         comments = []
         reply = []
-        
-        
+        comments.append("id" + "§" + "text" + "§" + "videoID")
+
         for item in response['items']:
             comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
             comment_id = item['snippet']['videoId']
             top_comment_id = item['id']
-            
-            
+
             request_replies = youtube.comments().list(
-            part="snippet",
-            maxResults=1,
-            parentId = top_comment_id
+                part="snippet",
+                maxResults=1,
+                parentId=top_comment_id
             )
             replies = request_replies.execute()
-            
+
             for x in replies['items']:
                 reply_snip = x['snippet']['textOriginal']
                 reply.append(reply_snip)
 
-            comments.append(comment+'§'+ comment_id)
-            #comments.append(reply_snip)
-        
-        commentthread.append(comments)   
-        
-    #print(top_comment_id)
+            comments.append(comment + '§' + comment_id)
+            comments.append(reply_snip + '§' + comment_id)
 
-  
+        commentthread.append(comments)
+
+    # print(top_comment_id)
+
     flattened = []
     for sublist in commentthread:
         for val in sublist:
             flattened.append(val)
 
-    #print(flattened)   
+    # print(flattened)
     out = pd.DataFrame.from_dict(flattened)
-  
-    out.to_csv('output2.csv', sep='\t', encoding='utf-8')
-    
-        
+    pd.DataFrame()
+
+    out.to_csv('output.csv', sep='§', encoding='utf-8')
+
+
 if __name__ == "__main__":
-    main()# -*- coding: utf-8 -*-
+    main()  # -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+# -*- coding: utf-8 -*-
 
 # Sample Python code for youtube.commentThreads.list
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "insert your Key"  # https://console.cloud.google.com
+    video_id = {""}  # enter one or more video ids
+
+    commentthread = []
+
+    for i in video_id:
+
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+        request = youtube.commentThreads().list(
+            part="snippet",
+            maxResults=10,
+            order="relevance",
+            textFormat="plainText",
+            videoId=i
+        )
+        response = request.execute()
+        print(response)
+        comments = []
+        reply = []
+        comments.append("id" + "§" + "text" + "§" + "videoID")
+
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comment_id = item['snippet']['videoId']
+            top_comment_id = item['id']
+
+            request_replies = youtube.comments().list(
+                part="snippet",
+                maxResults=1,
+                parentId=top_comment_id
+            )
+            replies = request_replies.execute()
+
+            for x in replies['items']:
+                reply_snip = x['snippet']['textOriginal']
+                reply.append(reply_snip)
+
+            comments.append(comment + '§' + comment_id)
+            comments.append(reply_snip + '§' + comment_id)
+
+        commentthread.append(comments)
+
+    # print(top_comment_id)
+
+    flattened = []
+    for sublist in commentthread:
+        for val in sublist:
+            flattened.append(val)
+
+    # print(flattened)
+    out = pd.DataFrame.from_dict(flattened)
+    pd.DataFrame()
+
+    out.to_csv('output.csv', sep='§', encoding='utf-8')
+
+
+if __name__ == "__main__":
+    main()  # -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
